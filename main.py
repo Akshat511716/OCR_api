7f182f996633615bb26525d5afa79510085d754c
Akshat Rana <akshat511716@gmail.com>
2023-03-14 22:26:31 +0530

    Change home page address

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43cc4899cac318c84d522474ba627dac1a764d579ec7240f09628494334bb0d5
-size 14283
+oid sha256:f79157b9bb915475e739297d5eced9f5d3507fb9893fd25f988e393bdfacdcae
+size 14321
