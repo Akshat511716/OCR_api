8375a4052ed96effc6eed25c3b73fbcdf6a65518
Akshat Rana <akshat511716@gmail.com>
2023-03-15 09:57:21 +0530

    Remove extra images that are being saved

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ab9b8dd7a57dacafb843783216e80cc0a9c851626872780a41da1b9c4d6ae77
-size 14325
+oid sha256:e3ccce6a2cf4559e0ab65f34464d4d1fa2ca6d831f3372b39538dbb9744a2545
+size 12497
